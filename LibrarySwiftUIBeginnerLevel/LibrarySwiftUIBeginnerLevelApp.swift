dbfeea7b95ca230ec396c615626a0802ea1cacc8
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-03-21 20:24:38 +0300

    Initial Commit

---
+++ LibrarySwiftUIBeginnerLevel/LibrarySwiftUIBeginnerLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibrarySwiftUIBeginnerLevelApp.swift
+//  LibrarySwiftUIBeginnerLevel
+//
+//  Created by Олейник Богдан on 21.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct LibrarySwiftUIBeginnerLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
